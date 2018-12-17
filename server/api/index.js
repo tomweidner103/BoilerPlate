f11aa233305b1aed990a197e07b934df63a0a253
Thomas Weidner <tomweidner103@gmail.com>
2018-12-17 12:55:31 -0800

    first commit

---
+++ server/api/index.js
@@ -0,0 +1,16 @@
+const router = require('express').Router()
+
+// routes for each model
+
+router.use()
+router.use()
+router.use()
+
+//error 404
+router.use((req,res,next) => {
+    const err = new Arror('Not found')
+    err.status = 404;
+    next(err)
+})
+
+module.exports = router
---
+++ server/index.js
@@ -0,0 +1,38 @@
+// entry point for server
+
+const path = require('path')
+const express = require('express')
+const app = express()
+const volleyball = require('volleyball')
+
+// logging middleware
+
+app.use(express.json())
+app.use(volleyball)
+
+// static middleware
+
+app.use(express.static(path.join(__dirname, '..', 'public')))
+
+// parsing middleware
+app.use(express.urlencoded({ extended: true }))
+
+//requiring the index.js in api folder
+
+app.use('/api', require('./api'))
+
+//send index.html
+
+app.get('*', (req,res,next) => {
+    res.sendFile(path.join(__dirname, '../public/index.html'))
+})
+
+// sending 500 errors
+
+app.use((err,req,res,next) =>{
+    console.error(err)
+    console.error(err.stack)
+    res.status(err.status || 500).send(err.message || "Internal server error.")
+})
+
+module.exports = app
